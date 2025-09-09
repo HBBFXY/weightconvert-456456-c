c9f570d283eb11fb4991fc138abddb8aaa1815c6
456456-c <1753340693@qq.com>
2025-09-09 16:43:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ unit = data[-2:]            # 单位部分
 
 if unit == "kg":
     pounds = weight * 2.2046
-    print(f"对应的英制重量为{pounds:.3f}磅")
+    print(f"对应的英制重量为{pounds:.3f}pd")
 elif unit == "pd":
     kilograms = weight / 2.2046
-    print(f"对应的公制重量为{kilograms:.3f}公斤")
+    print(f"对应的公制重量为{kilograms:.3f}kg")
 else:
     print("输入单位错误，请输入 kg 或 pd")
 
